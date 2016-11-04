0505b0f500d929e34eedc1c110f1e2fafc316996
James Byrne <jamesdesbyrne@gmail.com>
2016-11-04 15:52:58 +0000

    Adding two simple tests

--- test/controllers/donations_controller_test.rb
+++ test/controllers/donations_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
+require 'minitest/mock'
 
-class DonationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class DonationsControllerTest < ActionController::TestCase
+
+  test "should create a successful charge" do
+    post :create, params: {
+      amount: 200,
+      stripeToken: 1234567890
+    }
+
+    assert_response 200
+    json_result = JSON.parse(response.body)
+
+    assert_match "success", json_result["status"]
+  end
+
+  test "should render a card error" do
+    post :create, params: {
+      amount: 200.81,
+      stripeToken: 1234567890
+    }
+
+    assert_response 422
+    json_result = JSON.parse(response.body)
+
+    assert_not_nil json_result["type"]
+    assert_match "card_error", json_result["type"]
+  end
 end
